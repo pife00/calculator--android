9d9067586e7b93835f5c99ddde229383dfff62c0
pife00 <pife00@gmail.com>
2022-04-27 14:30:55 -0500

    I think that is complete

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import net.objecthunter.exp4j.Expression
 import net.objecthunter.exp4j.ExpressionBuilder
+
+private const val STATE_PENDING_OPERATION = "pedingOperation"
+private const val STATE_OPERAND1 ="Operand1"
+private const val STATE_OPERAND1_STORED = "Operand1_Stored"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var result : TextView;
@@ -66,7 +71,7 @@ class MainActivity : AppCompatActivity() {
 
         plus.setOnClickListener { operation.setText(operation.text.toString() + "+") }
         minus.setOnClickListener { operation.setText(operation.text.toString() +"-") }
-        multiply.setOnClickListener { operation.setText(operation.text.toString() +"*") }
+        multiply.setOnClickListener { operation.setText(operation.text.toString() +"x") }
         division.setOnClickListener { operation.setText(operation.text.toString() +"/") }
 
         equal.setOnClickListener {
@@ -75,6 +80,18 @@ class MainActivity : AppCompatActivity() {
             var result = expression.evaluate()
             operation.setText(result.toString())
         }
+    }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if(operation != null){
+            outState.putString(STATE_OPERAND1,operation.text.toString());
+        }
+        outState.putString(STATE_PENDING_OPERATION,"=")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        operation.text = savedInstanceState.getString(STATE_OPERAND1)
     }
 }
